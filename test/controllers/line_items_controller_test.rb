e9a3dba0b69a21b1beb8a674dc2b700aacf0526f
Anton Adigamov <AdigamovAntonAAA@gmail.com>
2020-05-13 16:06:50 +0200

    Group the same line items in a cart into one line.

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -17,13 +17,14 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create line_item" do
     assert_difference('LineItem.count') do
-      post line_items_url, params: {  product_id: products(:ruby).id }
+      post line_items_url, params: { product_id: products(:ruby).id }
     end
 
     follow_redirect!
 
     assert_select 'h2', 'Your Pragmatic Cart'
-    assert_select 'li', 'Programming Ruby 1.9'
+    assert_select 'body > section > main > ul > li:nth-child(1)',
+                  "1 \u00D7 Programming Ruby 1.9"
   end
 
   test "should show line_item" do
